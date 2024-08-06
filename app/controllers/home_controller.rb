cbadb88b79491d2de2978c6c718e7483ff0ef4a1
Dalen Ward <dalen.ward@m6securities.com>
2024-08-05 19:23:07 -0600

    got recipe data

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,44 @@
+class HomeController < ApplicationController
+  # skip authenticity token check
+  skip_before_action :verify_authenticity_token
+  def index
+    # respond with text
+    render plain: "Try posting to / with a recipe_url parameter to get a recipe back!"
+  end
+
+  def display
+    recipe_url = params[:recipe_url]
+
+    # make sure it's a valid url
+    if recipe_url.present? && recipe_url.match?(URI::DEFAULT_PARSER.make_regexp)
+      # get the recipe
+
+      response = RestClient.get(recipe_url)
+
+      if response.code != 200
+        # respond with an error message
+        render plain: "Invalid URL", status: :bad_request
+      else
+        # respond with the recipe
+        # render plain: response
+
+        html_body = Nokogiri::HTML(response.body)
+
+        # get this element's data
+        # <script type="application/ld+json">
+
+        content = html_body.css('script[type="application/ld+json"]').first.content
+
+        # parse content from json to hash
+
+        seo_data = JSON.parse(content)
+        @recipe_data = seo_data['@graph'].find { |data| data['@type'] == 'Recipe' }
+
+        puts JSON.pretty_generate(@recipe_data)
+      end
+    else
+      # respond with an error message
+      render plain: "Invalid URL", status: :bad_request
+    end
+  end
+end
